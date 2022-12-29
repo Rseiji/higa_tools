36df0beea5a406c413c9c4dab0eb031fc7dcc56d
Rseiji <uhrafael@gmail.com>
2022-12-29 20:09:45 -0300

    Adding installation of snippets extension

--- settings/jupyter_notebook/setup_enable.sh
+++ settings/jupyter_notebook/setup_enable.sh
@@ -10,3 +10,10 @@ while read line; do
     echo "Validating line " $line
     jupyter nbextension enable $line
 done < $filename
+
+# Manually installing snippets extension, due to some conflicts
+# https://jupyter-contrib-nbextensions.readthedocs.io/en/latest/nbextensions/snippets_menu/readme.html#:%7E:text=Sometimes%2C%20the%20menu,the%20real%20problem.
+git clone https://github.com/moble/jupyter_boilerplate
+jupyter nbextension install jupyter_boilerplate --user
+jupyter nbextension enable jupyter_boilerplate/main
+sudo rm -r jupyter_boilerplate
